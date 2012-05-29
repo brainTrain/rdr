64521c15ce3feadb6acce1cb20b4d0a34c7f3b22
Brian Schroeder <abductee@gmail.com>
2012-05-28 17:31:50 -0700

    slideshow esq stuff.  TODO: animation/easing

--- static/js/reddoorrules.js
+++ static/js/reddoorrules.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
+    var pos = $('#slide-0').position();
+    scroll(pos.left,pos.top);
+    
+    $('.splash-position, .full-list, .partial-list').click( function() {
+        var clickNumber = parseInt($(this).attr('number'), 10) + 1;
+        if(clickNumber == 15) {
+            clickNumber = 1;
+        } 
+        var $nextSlide = '#slide-' + clickNumber;
+        var pos = $($nextSlide).position();
+        $('body').animate(
+            {'left': '+=' + pos.left + 'px', 'top': '+=' + pos.top +'px'}, 
+        35000);
+        //scroll(pos.left,pos.top);
+    });
     
 });
