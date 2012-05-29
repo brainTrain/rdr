b6861445f5b55133891d70e077f84e6eb60819cc
root <root@philistine.brainnewstuff.com>
2012-05-28 23:23:32 -0700

    smooth scrolling to divs, scroll for full menu. TODO: dynamically fit/center divs

--- static/js/reddoorrules.js
+++ static/js/reddoorrules.js
@@ -3,18 +3,33 @@ $(document).ready(function() {
     scroll(pos.left,pos.top);
     
     $('.splash-position, .full-list, .partial-list').click( function() {
+        var windowWidth = $(window).width();
         var clickNumber = parseInt($(this).attr('number'), 10) + 1;
-        if(clickNumber == 15) {
+       
+        if(clickNumber == 2) {
+            $('body').css('overflow-y', 'hidden');
+        } else if(clickNumber == 15) {
             clickNumber = 1;
         } 
-        var $nextSlide = '#slide-' + clickNumber;
-        var pos = $($nextSlide).position();
-        /*
-        $('body').animate(
-            {'left': '+=' + pos.left + 'px', 'top': '+=' + pos.top +'px'}, 
-        2000);
-        */
-        scroll(pos.left,pos.top);
+
+        var $nextSlide = $('#slide-' + clickNumber);
+        var pos = $nextSlide.position();
+        var leftPos = pos.left;
+        var topPos = pos.top;
+
+        $('body').animate({
+            scrollLeft: leftPos + 'px', 
+            scrollTop: topPos +'px'
+        }, 1000);
+        $nextSlide.width(windowWidth);
+        if($nextSlide.hasClass('full-list')) {
+            $('body').css('overflow-y', 'scroll');
+        }
+        //$(this).removeClass('in-focus');
+        //$($nextSlide).addClass('in-focus');
+        //$(this).css('visibility', 'hidden');
+        //$nextSlide.css('visibility', 'visible');
+        //scroll(pos.left,pos.top);
     });
     
 });
