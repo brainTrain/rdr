9ff432903dccc75adff650098165067d0fdf27e2
root <root@philistine.brainnewstuff.com>
2012-05-31 01:05:24 -0700

    fiddlin

--- static/js/reddoorrules.js
+++ static/js/reddoorrules.js
@@ -5,7 +5,8 @@ $(document).ready(function() {
     $('.splash-position, .full-list, .partial-list').click( function() {
         var windowWidth = $(window).width();
         var clickNumber = parseInt($(this).attr('number'), 10) + 1;
-       
+        var divWidth = windowWidth * 0.7;
+        var divMargin = windowWidth * 0.2;
         if(clickNumber == 2) {
             $('body').css('overflow-y', 'hidden');
         } else if(clickNumber == 15) {
@@ -13,22 +14,24 @@ $(document).ready(function() {
         } 
 
         var $nextSlide = $('#slide-' + clickNumber);
-        var pos = $nextSlide.position();
-        var leftPos = pos.left;
-        var topPos = pos.top;
+        var pos = $nextSlide.offset();
+        var divHeight = $nextSlide.height(); 
+        var leftPos = pos.left - divMargin;
+        var topPos = pos.top - divHeight;
+            $nextSlide.width(divWidth);
 
         $('body').animate({
             scrollLeft: leftPos + 'px', 
             scrollTop: topPos +'px'
-        }, 1000);
-        $nextSlide.width(windowWidth);
+        }, 1000, function() { 
+        });
         if($nextSlide.hasClass('full-list')) {
             $('body').css('overflow-y', 'scroll');
         }
         //$(this).removeClass('in-focus');
         //$($nextSlide).addClass('in-focus');
-        //$(this).css('visibility', 'hidden');
-        //$nextSlide.css('visibility', 'visible');
+        $(this).css('visibility', 'hidden');
+        $nextSlide.css('visibility', 'visible');
         //scroll(pos.left,pos.top);
     });
     
